86bf0e6881bff97820ca47141b1e3f30749c0e9e
Vincent Hartline <elamrussvin@gmail.com>
2025-06-11 18:58:01 -0500

    Fixed Trigger
    Fixed Postgre sql trigger typo that prevented database initialization.

--- database/init.sql
+++ database/init.sql
@@ -26,7 +26,7 @@ CREATE INDEX IF NOT EXISTS idx_tasks_completed ON tasks(completed);
 
 -- Function to update updated_at timestamp
 CREATE OR REPLACE FUNCTION update_updated_at_column()
-RETURNS TRIGGER AS $
+RETURNS TRIGGER AS $$
 BEGIN
     NEW.updated_at = CURRENT_TIMESTAMP;
     RETURN NEW;
